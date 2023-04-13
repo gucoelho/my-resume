34a8f2ae94f51ade69c9ecb6649764934c518211
Gustavo Coelho <gu_coelho011@hotmail.com>
2023-04-13 12:53:01 -0300

    fix assetPath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+}
+
 const nextConfig = {
   output: 'export',
+  assetPrefix: assetPrefix,
   experimental: {
     appDir: true,
   },
